99fcb2afaa43ff2206a6e76b545a86cc4ce974b8
suskate1501 <suskatecyril_bsit@plmun.edu.ph>
2025-03-16 15:47:14 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 03

---
+++ university_db/delete_student.sql
@@ -0,0 +1,9 @@
+USE `UniversityDB`;
+
+
+DELETE FROM `Students`
+WHERE `FirstName` = 'Charlie' AND `LastName` = 'Lee'; -- More robust WHERE clause
+
+-- Alternative using StudentID (preferred if you know the ID):
+-- DELETE FROM `Students`
+-- WHERE `StudentID` = 3;  -- Replace 3 with Charlie's actual StudentID
